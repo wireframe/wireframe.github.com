c7760147ab154b806c5ed29460451a15a6923f84
Ryan Sonnek <ryan@codecrate.com>
2014-09-30 00:10:48 +0200

    Use local github metadata instead of javascript API access.

--- index.md
+++ index.md
@@ -17,4 +17,5 @@ My focuses also include: testing, REST API's, and pretty much any exciting web t
 I'm an avid opensource developer with a long history of involvement in the opensource community.
 The majority of my work can be found on [Github](https://github.com/wireframe).
 Some of my projects include:
-<div id="popular_repos"></div>
+
+<div id="popular_repositories"></div>
--- javascripts/github.js
+++ javascripts/github.js
@@ -1,10 +1,24 @@
+---
+---
 (function() {
+  // pull metadata about github repositories from jekyll
+  // see http://www.carsonshold.com/2014/05/github-metadata-with-jekyll-and-javascript/
+  var github = {{ site.github || jsonify }};
+
   // Put custom repo URL's in this object, keyed by repo name.
   var repoUrls = {
   };
 
+  // Put custom repo descriptions in this object, keyed by repo name.
+  var repoDescriptions = {
+  };
+
   function repoUrl(repo) {
     return repoUrls[repo.name] || repo.html_url;
+  }
+
+  function repoDescription(repo) {
+    return repoDescriptions[repo.name] || repo.description;
   }
 
   function addPopularRepo(repo) {
@@ -15,66 +29,46 @@
     }
     $link.append($("<span>").text(repo.name));
     $item.append($("<span>").text(' - ' + repoDescription(repo)));
-    $item.appendTo("#popular_repos");
-  }
-
-  // Put custom repo descriptions in this object, keyed by repo name.
-  var repoDescriptions = {
-  };
-
-  function repoDescription(repo) {
-    return repoDescriptions[repo.name] || repo.description;
+    $item.appendTo("#popular_repositories");
   }
 
   function addRepos(repos, page) {
-    repos = repos || [];
-    page = page || 1;
+    var repos = github.public_repositories;
 
-    var uri = "https://api.github.com/users/wireframe/repos?callback=?"
-            + "&per_page=100"
-            + "&page="+page;
+    // Convert pushed_at to Date.
+    $.each(repos, function (i, repo) {
+      repo.pushed_at = new Date(repo.pushed_at);
 
-    $.getJSON(uri, function (result) {
-      if (result.data && result.data.length > 0) {
-        repos = repos.concat(result.data);
-        addRepos(repos, page + 1);
-      } else {
-        $(function () {
-          // Convert pushed_at to Date.
-          $.each(repos, function (i, repo) {
-            repo.pushed_at = new Date(repo.pushed_at);
+      var weekHalfLife  = 1.146 * Math.pow(10, -9);
 
-            var weekHalfLife  = 1.146 * Math.pow(10, -9);
+      var pushDelta    = (new Date) - Date.parse(repo.pushed_at);
+      var createdDelta = (new Date) - Date.parse(repo.created_at);
 
-            var pushDelta    = (new Date) - Date.parse(repo.pushed_at);
-            var createdDelta = (new Date) - Date.parse(repo.created_at);
+      var weightForPush = 1;
+      var weightForWatchers = 10;
+      var weightForStars = 5;
 
-            var weightForPush = 1;
-            var weightForWatchers = 10;
-            var weightForStars = 5;
+      repo.hotness = 0;
+      repo.hotness += weightForPush * Math.pow(Math.E, -1 * weekHalfLife * pushDelta);
+      repo.hotness += weightForWatchers * repo.watchers;
+      repo.hotness += weightForStars * repo.stargazers_count;
+    });
 
-            repo.hotness = 0;
-            repo.hotness += weightForPush * Math.pow(Math.E, -1 * weekHalfLife * pushDelta);
-            repo.hotness += weightForWatchers * repo.watchers;
-            repo.hotness += weightForStars * repo.stargazers_count;
-          });
+    // Sort by popularity
+    repos.sort(function (a, b) {
+      if (a.hotness < b.hotness) return 1;
+      if (b.hotness < a.hotness) return -1;
+      return 0;
+    });
 
-          // Sort by popularity
-          repos.sort(function (a, b) {
-            if (a.hotness < b.hotness) return 1;
-            if (b.hotness < a.hotness) return -1;
-            return 0;
-          });
-
-          $.each(repos.slice(0, 10), function (i, repo) {
-            addPopularRepo(repo);
-          });
-        });
-      }
+    $.each(repos.slice(0, 10), function (i, repo) {
+      addPopularRepo(repo);
     });
   }
 
   $(function() {
-    addRepos();
+    if ($('#popular_repositories').length) {
+      addRepos();
+    }
   });
 })();
